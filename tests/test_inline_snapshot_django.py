b4739b20454d1e786d76d4ce4d8f46e09cd1db3c
Adam Johnson <me@adamj.eu>
2025-06-04 10:02:30 +0100

    Capture for all database connections by default

--- tests/test_inline_snapshot_django.py
+++ tests/test_inline_snapshot_django.py
@@ -1,14 +1,72 @@
 from __future__ import annotations
 
+import inspect
+from textwrap import dedent
+
+import django
+import pytest
 from django.test import TestCase
+from django.test.utils import CaptureQueriesContext
 from inline_snapshot import snapshot
 
 from inline_snapshot_django import snapshot_queries
 from tests.models import Character
 
 
-class IndexTests(TestCase):
+class SnapshotQueriesTests(TestCase):
     databases = {"default", "other"}
+
+    @pytest.mark.skipif(django.VERSION < (5, 2), reason="Django 5.2+")
+    def test_capture_queries_context_source(self):
+        """
+        snapshot_queries() copied a lot of code from the upstream
+        CaptureQueriesContext, so check for any upstream changes that may be
+        worth copying over.
+        """
+        source = dedent(inspect.getsource(CaptureQueriesContext))
+        expected = dedent(
+            '''\
+            class CaptureQueriesContext:
+                """
+                Context manager that captures queries executed by the specified connection.
+                """
+
+                def __init__(self, connection):
+                    self.connection = connection
+
+                def __iter__(self):
+                    return iter(self.captured_queries)
+
+                def __getitem__(self, index):
+                    return self.captured_queries[index]
+
+                def __len__(self):
+                    return len(self.captured_queries)
+
+                @property
+                def captured_queries(self):
+                    return self.connection.queries[self.initial_queries : self.final_queries]
+
+                def __enter__(self):
+                    self.force_debug_cursor = self.connection.force_debug_cursor
+                    self.connection.force_debug_cursor = True
+                    # Run any initialization queries if needed so that they won't be
+                    # included as part of the count.
+                    self.connection.ensure_connection()
+                    self.initial_queries = len(self.connection.queries_log)
+                    self.final_queries = None
+                    request_started.disconnect(reset_queries)
+                    return self
+
+                def __exit__(self, exc_type, exc_value, traceback):
+                    self.connection.force_debug_cursor = self.force_debug_cursor
+                    request_started.connect(reset_queries)
+                    if exc_type is not None:
+                        return
+                    self.final_queries = len(self.connection.queries_log)
+            '''
+        )
+        assert source == expected
 
     def test_single_query(self):
         with snapshot_queries() as snap:
@@ -30,8 +88,25 @@ class IndexTests(TestCase):
             ]
         )
 
-    def test_other_database(self):
-        with snapshot_queries(using="other") as snap:
+    def test_multiple_databases(self):
+        with snapshot_queries() as snap:
+            Character.objects.using("default").count()
+            Character.objects.using("other").count()
+            Character.objects.using("default").count()
+            Character.objects.using("other").count()
+
+        assert snap == snapshot(
+            [
+                "SELECT ... FROM tests_character",
+                ("other", "SELECT ... FROM tests_character"),
+                "SELECT ... FROM tests_character",
+                ("other", "SELECT ... FROM tests_character"),
+            ]
+        )
+
+    def test_using_default_database(self):
+        with snapshot_queries(using="default") as snap:
+            Character.objects.count()
             Character.objects.using("other").count()
         assert snap == snapshot(
             [
@@ -39,7 +114,16 @@ class IndexTests(TestCase):
             ]
         )
 
-    def test_other_database_unused(self):
+    def test_using_other_database(self):
+        with snapshot_queries(using="other") as snap:
+            Character.objects.using("other").count()
+        assert snap == snapshot(
+            [
+                ("other", "SELECT ... FROM tests_character"),
+            ]
+        )
+
+    def test_using_other_database_unused(self):
         with snapshot_queries(using="other") as snap:
             Character.objects.count()
         assert snap == snapshot([])
